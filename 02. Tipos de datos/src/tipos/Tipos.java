d054a3a89974c90713c2015f4ad210e7c59eaecc
Moises Sanchez <moisesup.25@gmail.com>
2024-04-09 16:59:21 -0500

    tipos

---
+++ 02. Tipos de datos/src/tipos/Tipos.java
@@ -0,0 +1,24 @@
+package tipos;
+
+public class Tipos {
+    public static void main(String[] args) {
+        //1.Numericos
+        //1.1 Enteros
+        byte variable1 = 5;
+        short variable2 = 10;
+        int variable3 = 30;
+        long variable4 = 100;
+        //1.2 Decimal
+        float variable5 = 26.5f;
+        double variable6 = 10.5d;
+        //2. Booleano
+        boolean variable7 = false;
+        boolean variable8 = true;
+        //3. Texto
+        String variable9 = "Moises";
+        char variable11 = 'a';
+        //4. Constantes
+        final int variable10 = 15;
+        //variable10 = 18; //Nos saldrá un error ya que la variable 10 es una constante y no se puede cambiar.
+    }
+}
