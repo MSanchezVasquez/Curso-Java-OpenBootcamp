34b36aadbb49330f1a4dd53efb5423911eb4eb64
Moises Sanchez <moisesup.25@gmail.com>
2024-04-09 16:19:40 -0500

    holamundo

---
+++ 01.HolaMundo/src/co/moisessanchez/HolaMundo.java
@@ -0,0 +1,7 @@
+package co.moisessanchez;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo desde sesión 1 de OpenBootcamp");
+    }
+}
