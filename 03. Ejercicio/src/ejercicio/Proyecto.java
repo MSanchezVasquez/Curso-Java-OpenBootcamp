986e17730299851aac7c62eeb471075e30aa66dd
Moises Sanchez <moisesup.25@gmail.com>
2024-04-09 17:07:42 -0500

    ejercicio

---
+++ 03. Ejercicio/src/ejercicio/Proyecto.java
@@ -0,0 +1,10 @@
+package ejercicio;
+
+public class Proyecto {
+    public static void main(String[] args) {
+        System.out.println("Hola soy Moises.");
+        System.out.println("Tengo "+18+ " años.");
+        System.out.println("Mido "+1.95+" metros de altura.");
+        System.out.println("Soltero: "+true);
+    }
+}
